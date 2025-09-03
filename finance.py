dcbef1b1bdfeff617abb7f5e4e71e8553bb95208
frrst-ian <frrstxd@proton.me>
2025-09-03 22:48:01 +0800

    feat: implement summary sub-command that prints hard coded values

--- finance.py
+++ finance.py
@@ -1,2 +1,19 @@
 import argparse
 
+
+def main():
+    parser = argparse.ArgumentParser(description="Personal Finance Tracker")
+    subparsers = parser.add_subparsers(dest="command")
+
+    summary_parser = subparsers.add_parser('summary', help="Show financial summary")
+
+    args = parser.parse_args()
+
+    if(args.command == 'summary'):
+        print("Total Income: $0")
+        print("Total Expenses: $0")
+        print("Balance: $0")
+
+
+if __name__ == "__main__":
+    main()
